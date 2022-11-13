f8e860d483f5589d7e8f77993749f603b610b227
s-weigand <s.weigand.phy@gmail.com>
2022-11-13 13:13:15 +0100

    download

--- biber_manager/download.py
+++ biber_manager/download.py
@@ -7,15 +7,22 @@ from requests_html import HTMLSession
 
 BASE_URL = "https://sourceforge.net"
 FILES_URL = f"{BASE_URL}/projects/biblatex-biber/files/biblatex-biber"
+DOC_TEX_URL = "https://raw.githubusercontent.com/plk/biber/dev/doc/biber.tex"
 
 VALID_VERSION_PATTERN = re.compile(r"^(\d+\.\d+(\.\d+)?|current)$")
+VERSION_TABLE_BODY_PATTER = re.compile(
+    r"\\midrule\n(?P<rows>.+?)\\bottomrule.+\\caption\{\\biber\/\\biblatex\\ compatibility matrix",
+    re.DOTALL,
+)
+VERSION_TABLE_VERSION_PATTER = re.compile(
+    r"(?P<biber_version>\d\.[.\d]+) & (?P<biblatex_version>\d\.[.\d]+)\\\\\n"
+)
 
 VERSION_DOWNLOAD_EXTENSION = {
     "win32": "binaries/Windows/biber-MSWIN64.zip",
     "cygwin": "binaries/Cygwin/biber-cygwin64.tar.gz",
     "linux": "binaries/Linux/biber-linux_x86_64.tar.gz",
     "darwin": "binaries/MacOS/biber-darwin_universal.tar.gz",
-    "freebsd": "binaries/FreeBSD/biber-amd64-freebsd.tar.xz",
 }
 
 
@@ -58,3 +65,15 @@ def find_valid_biber_versions() -> dict[str, str]:
                 version_dict[version_str] = f"{BASE_URL}{link.attrs['href']}"
 
     return version_dict
+
+
+def fetch_biber_biblatex_version_table() -> list[dict[str, str]]:
+
+    session = HTMLSession()
+    resp = session.get(DOC_TEX_URL)
+
+    rows_match = VERSION_TABLE_BODY_PATTER.search(resp.text)
+    if rows_match is None:
+        raise ValueError("Could not find table pattern.")
+
+    return [row.groupdict() for row in VERSION_TABLE_VERSION_PATTER.finditer(rows_match["rows"])]
